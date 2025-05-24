b812af79e5f094b0a68d61cd68d362b601c58d3a
Pujan-khunt <pujankhunt2412@gmail.com>
2025-05-24 12:57:49 +0530

    chore: create utility for connecting mongoDb with mongoose

---
+++ src/config/connectDB.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose"
+
+const MONGODB_URI = process.env.MONGODB_URI;
+
+const connectDB = async () => {
+  try {
+    const response = await mongoose.connect(MONGODB_URI);
+    console.log("MongoDB Connected Successfully, Maybe ;)", response);
+  } catch (error) {
+    console.error("MongoDB Connection Failure")
+    if (error instanceof Error) {
+      console.error("Error Message: ", error.message)
+    } else {
+      console.error("Unexpected Error", error)
+    }
+    process.exit(1)
+  }
+}
+
+export default connectDB;
